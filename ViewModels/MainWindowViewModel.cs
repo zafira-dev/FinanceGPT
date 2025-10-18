a55417bfd587350db1780ef8ffeb5440fd6e0d62
zaf121 <zafrei24@student.loreto.ac.uk>
2025-10-18 09:45:19 +0100

    commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace FinanceGPT.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string currentPage = "Dashboard";
+
+        public void NavigateTo(string page)
+        {
+            CurrentPage = page;
+        }
+    }
+}
